cafe58fbf543be23204cfbf59d48300b0361b922
Eser Kuru <eser.kuru@infoline-tr.com>
2019-09-18 14:37:52 +0300

    Initial commit

---
+++ CodeDom2/MyGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.CSharp;
+
+namespace CodeDom2
+{
+    public static class MyGenerator
+    {
+        public static void Test()
+        {
+
+            CodeCompileUnit lUnit = new CodeCompileUnit();
+            CodeNamespace lNamespace = new CodeNamespace("CodeDom2");
+
+            lNamespace.Imports.Add(new CodeNamespaceImport("System.IO"));
+            lUnit.Namespaces.Add(lNamespace);
+
+            CodeTypeDeclaration lClass = new CodeTypeDeclaration("MyClass");
+            CodeMethodInvokeExpression lExpression = new CodeMethodInvokeExpression(new CodeTypeReferenceExpression("Console"), "WriteLine", new CodePrimitiveExpression("hello world !"));
+            lNamespace.Types.Add(lClass);
+
+            // write Main entry point method
+            CodeEntryPointMethod lMain = new CodeEntryPointMethod();
+            lMain.Statements.Add(lExpression);
+            lClass.Members.Add(lMain);
+
+            // write another method
+            CodeMemberMethod lMethod = new CodeMemberMethod();
+            lMethod.Name = "DoSomething";
+            lClass.Members.Add(lMethod);
+
+            //string lDesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\";
+            string lSolutionPath =
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()))) + @"\";
+            CodeGeneratorOptions lOptions = new CodeGeneratorOptions();
+            lOptions.IndentString = "  "; // or "\t";
+            lOptions.BlankLinesBetweenMembers = true;
+
+            // generate a C# source code file
+            CSharpCodeProvider lCSharpCodeProvider = new CSharpCodeProvider();
+            using (StreamWriter lStreamWriter = new StreamWriter(lSolutionPath + "HelloWorld.cs", false))
+            {
+                lCSharpCodeProvider.GenerateCodeFromCompileUnit(lUnit, lStreamWriter, lOptions);
+            }
+        }
+    }
+}
---
+++ CodeDom2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace CodeDom2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGenerator.Test();
+        }
+    }
+}
